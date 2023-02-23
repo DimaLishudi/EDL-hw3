631d26afc7e49a0efa9fc11ffb6b1c0c4c2b05a5
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2023-02-24 02:31:59 +0300

    fix dataset link

--- section2/download_data.sh
+++ section2/download_data.sh
@@ -1,4 +1,4 @@
-wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
 mkdir data
 unzip wikitext-103-raw-v1.zip -d data
 rm wikitext-103-raw-v1.zip
