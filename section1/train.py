7362a18ad4ac0118255d441ccd1d1f5e54a22936
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2023-02-23 19:49:11 +0300

    refactor gradient scaling into separate class

--- section1/train.py
+++ section1/train.py
@@ -6,24 +6,61 @@ from section1.unet import Unet
 from section1.dataset import get_train_data
 from typing import List
 
-# https://pytorch.org/docs/stable/amp.html#gradient-scaling
-# добавил возращение списка коэффициентов скалирования лосса для динамического скалирования
+class GradScaler():
+    def __init__(
+            self,
+            scaling_type: str="None",
+            loss_scale: float=2**16,
+            up_scale: float=2,
+            down_scale: float=2,
+            up_scale_freq: int=5,
+        ):
+        self.scaling_type = scaling_type
+        self.loss_scale = loss_scale
+        self.up_scale = up_scale
+        self.down_scale = down_scale
+        self.up_scale_freq = up_scale_freq
+        self.loss_scale_list = []
+        return self
+
+
+    def step(self, optimizer, loss):
+        if self.scaling_type != "const" and self.scaling_type != "dynamic":
+            loss.backward()
+            optimizer.step()
+            return
+
+        if self.scaling_type =="dynamic":
+            self.loss_scale_list.append(self.loss_scale)
+        loss *= self.loss_scale
+        loss.backward()
+        with torch.no_grad():
+            for p_group in optimizer.param_groups:
+                for p in p_group["params"]:
+                    if not torch.isfinite(p.grad).all():
+                        if self.scaling_type == "dynamic":
+                            good_grads_counter = 0
+                            loss_scale /= self.down_scale
+                        return
+                    p.grad /= self.loss_scale
+        if self.scaling_type == "dynamic":
+            good_grads_counter += 1
+            if good_grads_counter == self.up_scale_freq:
+                self.loss_scale *= self.up_scale
+                good_grads_counter = 0
+        optimizer.step()
+        loss /= self.loss_scale
+        
+
 def train_epoch(
     train_loader: torch.utils.data.DataLoader,
     model: torch.nn.Module,
     criterion: torch.nn.modules.loss._Loss,
     optimizer: torch.optim.Optimizer,
     device: torch.device,
-    scaling_type: str="None",
-    loss_scale: float=2**16,
-    up_scale: float=2,
-    down_scale: float=2,
-    up_scale_freq: int=5,
-) -> List[float]:   
+    scaler: GradScaler,
+) -> None:   
     model.train()
-    loss_scale_list = []
-    good_grads_counter = 0
-
     pbar = tqdm(enumerate(train_loader), total=len(train_loader))
 
     for i, (images, labels) in pbar:
@@ -34,41 +71,13 @@ def train_epoch(
         with torch.cuda.amp.autocast():
             outputs = model(images)
             loss = criterion(outputs, labels)
-        
-        if scaling_type == "const" or scaling_type == "dynamic":
-            loss *= loss_scale
-            loss.backward()
-            bad_grads = False
-            with torch.no_grad():
-                for p_group in optimizer.param_groups:
-                    for p in p_group["params"]:
-                        if not torch.isfinite(p.grad).all():
-                            bad_grads = True
-                            break
-                        p.grad /= loss_scale
-                    if bad_grads:
-                        break
-            if scaling_type == "dynamic":
-                loss_scale_list.append(loss_scale)
-                if bad_grads:
-                    loss_scale /= down_scale
-                    good_grads_counter = 0
-                else:
-                    good_grads_counter += 1
-                    if good_grads_counter == up_scale_freq:
-                        loss_scale *= up_scale
-                        good_grads_counter = 0
-            if not bad_grads:
-                optimizer.step()
-            loss /= loss_scale
-        else:
-            loss.backward()
-            optimizer.step()
+
+        scaler.step(optimizer, loss)
 
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
         pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
-    return loss_scale_list
+
 
 def train(seed=0, **kwargs):
     torch.manual_seed(seed)
@@ -79,8 +88,8 @@ def train(seed=0, **kwargs):
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
     train_loader = get_train_data()
-    scales = []
+    scaler = GradScaler(**kwargs)
     num_epochs = 5
     for epoch in range(0, num_epochs):
-        scales += train_epoch(train_loader, model, criterion, optimizer, device=device, **kwargs)
-    return scales
+        train_epoch(train_loader, model, criterion, optimizer, device=device, scaler=scaler)
+    return scaler.loss_scale_list
