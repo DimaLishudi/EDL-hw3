f490c33e9cdd05316abaa73dd56d898abe5f95e2
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2023-02-24 03:28:18 +0300

    data

--- section2/dataset.py
+++ section2/dataset.py
@@ -12,7 +12,12 @@ class BrainDataset(Dataset):
     def __init__(self, data_path: str="data/wikitext-103-raw/", max_length: int = MAX_LENGTH):
         tokenizer = get_tokenizer("basic_english")
         raw_texts = []
-        with open(data_path + "wiki.train.raw") as f:
+        with open(data_path + "wiki.valid.raw") as f:
+            for line in f:
+                line = line.strip()
+                if line != "":
+                    raw_texts.append(tokenizer(line))
+        with open(data_path + "wiki.test.raw") as f:
             for line in f:
                 line = line.strip()
                 if line != "":
@@ -41,7 +46,12 @@ class BigBrainDataset(Dataset):
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
         tokenizer = get_tokenizer("basic_english")
         raw_texts = []
-        with open(data_path + "wiki.train.raw") as f:
+        with open(data_path + "wiki.valid.raw") as f:
+            for line in f:
+                line = line.strip()
+                if line != "":
+                    raw_texts.append(tokenizer(line))
+        with open(data_path + "wiki.test.raw") as f:
             for line in f:
                 line = line.strip()
                 if line != "":
@@ -70,7 +80,12 @@ class UltraDuperBigBrainDataset(Dataset):
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH, n_bins: int = 1):
         tokenizer = get_tokenizer("basic_english")
         raw_texts = []
-        with open(data_path + "wiki.train.raw") as f:
+        with open(data_path + "wiki.valid.raw") as f:
+            for line in f:
+                line = line.strip()
+                if line != "":
+                    raw_texts.append(tokenizer(line))
+        with open(data_path + "wiki.test.raw") as f:
             for line in f:
                 line = line.strip()
                 if line != "":
